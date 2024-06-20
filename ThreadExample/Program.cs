3eef7733db29ea629ddd5ec4cee54acc5c025470
saurabh <saurabh908@outlook.com>
2024-06-20 18:52:03 +0530

    Basic thread setup

---
+++ ThreadExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading;
+
+Thread newThreadForPrintingName = new Thread(PrintName);
+newThreadForPrintingName.Start();
+
+for (int i = 0; i < 1000; i++) Console.Write("Mona");
+
+void PrintName()
+{
+    for (int i = 0; i < 1000; i++) Console.Write("Pradyumna");
+}

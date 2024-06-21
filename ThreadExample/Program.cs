ba628d7ccb418cd93029bb5cbd475b96303aae7c
saurabh <saurabh908@outlook.com>
2024-06-21 19:32:10 +0530

    Thread end scenario

--- ThreadExample/Program.cs
+++ ThreadExample/Program.cs
@@ -4,9 +4,28 @@ using System.Threading;
 Thread newThreadForPrintingName = new Thread(PrintName);
 newThreadForPrintingName.Start();
 
-for (int i = 0; i < 1000; i++) Console.Write("_Mona_");
+for (int i = 0; i < 1000; i++)
+{
+    Console.Write("_Mona_");
+    Console.Write("_Start_T_M_", Thread.CurrentThread.Name,"_End");
+}
 
 void PrintName()
 {
-    for (int i = 0; i < 1000; i++) Console.Write("_Pradyumna_");
+    for (int i = 0; i < 1000; i++)
+    {
+        Console.Write("_Pradyumna_");
+        Console.Write("_Start_T_P_", Thread.CurrentThread.Name,"_End");
+    }
+}
+newThreadForPrintingName.Join();
+
+Thread t = new Thread(Go);
+t.Start();
+t.Join();
+Console.WriteLine("Thread t has ended!");
+
+void Go()
+{
+    for (int i = 0; i < 1000; i++) Console.Write("y");
 }
